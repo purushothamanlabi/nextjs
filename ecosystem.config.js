90451be56604866131a2146b602aef2317c1bca7
purushothamanlabi <purushothaman.abi.2003@gmail.com>
2026-05-18 13:22:21 +0530

    feat: add data configuration and migrate ecosystem config to ESM syntax

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,11 @@
-module.exports = {
-  apps: [
-    {
-      name: "nextjs-app",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
-      env: {
-        PORT: 3000,
-        NODE_ENV: "production"
-      }
+export const apps = [
+  {
+    name: "nextjs-app",
+    script: "node_modules/next/dist/bin/next",
+    args: "start",
+    env: {
+      PORT: 3000,
+      NODE_ENV: "production"
     }
-  ]
-};
+  }
+];
